e18778f0ace4f08343a9737c331c45f1f1782176
Sharon Natalia <sharon.natalia@anabatic.com>
2020-11-08 15:50:48 +0700

    datatable

--- application/views/admin/faq.php
+++ application/views/admin/faq.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>PROTECH</title>
+  <link rel="stylesheet" href="/Protech_BE/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+    
+    <link href="/Protech_BE/assets/css/pages/reports.css" rel="stylesheet">
 </head>
 <body>
 <?php require 'application/views/header.php'; ?>
@@ -14,14 +17,11 @@
     <div class="container">
       <div class="row">
         <div class="span12">
-          <div class="widget widget-table action-table">
-            <div class="widget-header"> <i class="icon-th-list"></i>
+          <div class="info-box">
+               <div class="row-fluid stats-box">
               <h3>FAQ Master</h3>
               <a class="btn btn-invert" href="/Protech_BE/index.php/Controller_FAQ/createFAQ">+</a>
-            </div>
-            <!-- /widget-header -->
-            <div class="widget-content">
-                    <table class="table table-bordered data-table">
+            <table class="table table-bordered data-table">
                       <thead>
                         <tr>
                           <th style="text-align: center">ID</th>
@@ -57,6 +57,51 @@
                         ?>
                       </tbody>
                     </table>
+                  </div>
+          </div>
+          <div class="widget widget-table action-table">
+            <div class="widget-header"> <i class="icon-th-list"></i>
+              <h3>FAQ Master</h3>
+              <a class="btn btn-invert" href="/Protech_BE/index.php/Controller_FAQ/createFAQ">+</a>
+            </div>
+            <!-- /widget-header -->
+            <div class="widget-content">
+                <table class="table table-bordered data-table">
+                      <thead>
+                        <tr>
+                          <th style="text-align: center">ID</th>
+                          <th style="text-align: center">Question</th>
+                          <th style="text-align: center">Answer</th>
+                          <th style="text-align: center">Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php 
+                          $no=0;
+                          foreach ($data as $list_faq){
+                          $no++;
+                        ?>
+                          <tr>
+                              <td><?=$no?></td>
+                              <td><?=$list_faq->faq_question?></td>
+                              <td><?=$list_faq->faq_answer?></td>
+                              <td style="text-align: center">
+                                 <a class="btn btn-info" href="/Protech_BE/index.php/Controller_FAQ/getOne/<?=$list_faq->id?>" data-toggle="tooltip" title="View" style="padding: 4px">
+                                  <i class="icon-eye-open"></i>
+                                </a>
+                                 <a class="btn btn-warning" href="/Protech_BE/index.php/Controller_FAQ/updateFAQ/<?=$list_faq->id?>" data-toggle="tooltip" title="Edit" style="padding: 4px">
+                                  <i class="icon-pencil"></i>
+                                </a>
+                                 <a class="btn btn-danger" href="/Protech_BE/index.php/Controller_FAQ/deleteFAQ/<?=$list_faq->id?>" data-toggle="tooltip" title="Delete" style="padding: 4px">
+                                  <i class="icon-trash"></i>
+                                </a>
+                              </td>
+                          </tr>
+                        <?php
+                        }
+                        ?>
+                      </tbody>
+                    </table>    
             </div>
             <!-- /widget-content --> 
           </div>
@@ -69,7 +114,14 @@
   </div>
   <!-- /main-inner --> 
 </div>
+  <script src="/Protech_BE/assets/vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="/Protech_BE/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
 <?php require 'application/views/extra.php'; ?>
 <?php require 'application/views/footer.php'; ?>
+  <script type="text/javascript">
+    $(document).ready(function(){  
+      $('.data-table').dataTable();      
+    });
+  </script>
 </body>
 </html>

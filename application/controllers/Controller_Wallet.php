55cdd06743727b3f9aea13eb2233b7e85d1bb2da
Sharon Natalia <sharon.natalia@anabatic.com>
2020-12-17 00:58:37 +0700

    path

--- application/controllers/Controller_Wallet.php
+++ application/controllers/Controller_Wallet.php
@@ -69,8 +69,8 @@ class Controller_Wallet extends CI_Controller{
 	        
 	        $filename = 'transaction_history_report_'.date("Ymdhis").'.pdf';
             
-	        $pdf->Output('S:/Program Files/xampp/htdocs/protech/assets/downloaded-pdf/'.$filename,'F');
-// 	        $pdf->Output('E:/xampp/htdocs/protech/assets/downloaded-pdf/'.$filename,'F');
+//	        $pdf->Output('S:/Program Files/xampp/htdocs/protech/assets/downloaded-pdf/'.$filename,'F');
+ 	        $pdf->Output('E:/xampp/htdocs/protech/assets/downloaded-pdf/'.$filename,'F');
 	        force_download('./assets/downloaded-pdf/'.$filename,NULL);
 	    }
 	}

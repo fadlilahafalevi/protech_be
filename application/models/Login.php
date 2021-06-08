17ab18486ac0724fec8d22f8145e5802328e4ff2
Fadlilah Achmad Falevi <fadlilah21.achmad@gmail.com>
2021-06-08 21:22:47 +0700

    db login

--- application/models/Login.php
+++ application/models/Login.php
@@ -2,7 +2,7 @@
 class Login extends CI_Model{
     public function cekadmin($email,$password){
         $hasil=$this->db->query("select ul.user_code, ul.role_id, email, concat(coalesce(first_name,''), ' ', coalesce(middle_name,''), ' ', coalesce(last_name,'')) as fullname, phone,
-        	is_verified, email 
+        	is_verified, email, ul.active_status
 			from tbl_user_login ul 
 			join tbl_user_profile up on up.user_code = ul.user_code
 			join tbl_user_role ur on ur.role_id = ul.role_id

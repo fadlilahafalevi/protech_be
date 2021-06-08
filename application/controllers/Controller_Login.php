24e2d4e9468adf2c0f4fada836991fada053a5e2
Fadlilah Achmad Falevi <fadlilah21.achmad@gmail.com>
2021-06-08 18:54:12 +0700

    update login message

--- application/controllers/Controller_Login.php
+++ application/controllers/Controller_Login.php
@@ -21,88 +21,69 @@ class Controller_Login extends CI_Controller{
         $password   = strip_tags(stripslashes($this->input->post('password',TRUE)));
 
         $cadmin = $this->Login->cekadmin($email,$password);
-
-        $is_email_exist = $this->M_General->check_existing_email($email);
-        if ($is_email_exist == false) {
-            $url=base_url('Controller_Login');
-            echo $this->session->set_flashdata('msg','User tidak aktif');
-            redirect($url);
-        }
         
         if($cadmin->num_rows() > 0){
-            $this->session->set_userdata('masuk', true);
+            if ($active_status == 1) {
+                $this->session->set_userdata('masuk', true);
 
-            $xcadmin = $cadmin->row_array();
-            
-            if($xcadmin['role_id'] == '1'){ //superadmin
-                $this->session->set_userdata('akses', '1');
-                $user_code    = $xcadmin['user_code'];
-                $user_name    = $xcadmin['fullname'];
-                $this->session->set_userdata('user_code', $user_code);
-                $this->session->set_userdata('user_name', $user_name);
-            } elseif($xcadmin['role_id'] == '2'){ //admin
-                $this->session->set_userdata('akses', '2');
-                $user_code    = $xcadmin['user_code'];
-                $user_name    = $xcadmin['fullname'];
-                $this->session->set_userdata('user_code', $user_code);
-                $this->session->set_userdata('user_name', $user_name);
-            } elseif($xcadmin['role_id'] == '3'){ //teknisi
-                $this->session->set_userdata('akses', '3');
-                $user_code    = $xcadmin['user_code'];
-                $user_name    = $xcadmin['fullname'];
-                $this->session->set_userdata('user_code', $user_code);
-                $this->session->set_userdata('user_name', $user_name);
-            } elseif($xcadmin['role_id'] == '4'){ //pelanggan
-                $this->session->set_userdata('akses', '4');
-                $user_code    = $xcadmin['user_code'];
-                $user_name    = $xcadmin['fullname'];
-                $this->session->set_userdata('user_code', $user_code);
-                $this->session->set_userdata('user_name', $user_name);
+                $xcadmin = $cadmin->row_array();
+                
+                if($xcadmin['role_id'] == '1'){ //superadmin
+                    $this->session->set_userdata('akses', '1');
+                    $user_code    = $xcadmin['user_code'];
+                    $user_name    = $xcadmin['fullname'];
+                    $this->session->set_userdata('user_code', $user_code);
+                    $this->session->set_userdata('user_name', $user_name);
+                } elseif($xcadmin['role_id'] == '2'){ //admin
+                    $this->session->set_userdata('akses', '2');
+                    $user_code    = $xcadmin['user_code'];
+                    $user_name    = $xcadmin['fullname'];
+                    $this->session->set_userdata('user_code', $user_code);
+                    $this->session->set_userdata('user_name', $user_name);
+                } elseif($xcadmin['role_id'] == '3'){ //teknisi
+                    $this->session->set_userdata('akses', '3');
+                    $user_code    = $xcadmin['user_code'];
+                    $user_name    = $xcadmin['fullname'];
+                    $this->session->set_userdata('user_code', $user_code);
+                    $this->session->set_userdata('user_name', $user_name);
+                } elseif($xcadmin['role_id'] == '4'){ //pelanggan
+                    $this->session->set_userdata('akses', '4');
+                    $user_code    = $xcadmin['user_code'];
+                    $user_name    = $xcadmin['fullname'];
+                    $this->session->set_userdata('user_code', $user_code);
+                    $this->session->set_userdata('user_name', $user_name);
+                }
+            } else if ($active_status == 0) {
+                redirect('Controller_Login/user_inactive');
             }
+        } else {
+            redirect('Controller_Login/login_failed');
         }
         
-        if($this->session->userdata('masuk') == true){
+        if($this->session->userdata('masuk') == true) {
 
             //update last login
             $data = ['last_login_datetime' => date("Y-m-d H:i:s")];
             $this->M_General->updateData('tbl_user_login', $data, 'user_code', $this->session->userdata('user_code'));
 
             if($this->session->userdata('akses')=='1'){
-                if ($active_status == 1) {
-                    redirect('Controller_Dashboard');
-                } else if ($active_status == 0) {
-                   redirect('Controller_Login/user_inactive');
-                }
+                redirect('Controller_Dashboard');
             } 
             else if($this->session->userdata('akses')=='2') {
-                if ($active_status == 1) {
-                    redirect('Controller_Dashboard');
-                } else if ($active_status == 0) {
-                   redirect('Controller_Login/user_inactive');
-                }
+                redirect('Controller_Dashboard');
             }
             else if($this->session->userdata('akses')=='3') {
-                if ($active_status == 1) {
-                    redirect('Controller_DashboardTechnician');
-                } else if ($active_status == 0) {
-                   redirect('Controller_Login/user_inactive');
-                }
+                redirect('Controller_DashboardTechnician');
             }
             else if($this->session->userdata('akses')=='4') {
                 $is_verified = $xcadmin['is_verified'];
                 $active_status = $xcadmin['active_status'];
-                
                 if ($is_verified == 0) {
                     redirect('Controller_Login/account_not_verified');
                 } else if ($is_verified == 1 && $active_status == 1) {
                     redirect('Controller_DashboardCustomer');
-                } else if ($active_status == 0) {
-                   redirect('Controller_Login/user_inactive');
                 }
-                
             }
-        }else{
-            redirect('Controller_Login/login_failed');
         }
     }
 
@@ -113,7 +94,6 @@ class Controller_Login extends CI_Controller{
     }
 
     function user_inactive() {
-        $this->session->sess_destroy();
         $url=base_url('Controller_Login');
         echo $this->session->set_flashdata('msg','User tidak aktif');
         redirect($url);
